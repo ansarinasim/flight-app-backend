3d8311aaff3ad87439843c3b441b5e994838c929
ansarinasim <112812721+ansarinasim@users.noreply.github.com>
2023-10-28 14:06:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,8 +28,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    // origin: "http://localhost:4200",
-    origin: "https://ansarinasim.github.io/demo-flight-booking-app/",
+    origin: "http://localhost:4200",
   })
 );
 // app.use("*", function (req, res) {
